5c015ae02d62a76919b225730a0a27c68bb20012
tyenki <tyenki@gmail.com>
2011-12-11 15:58:12 +1100

    Removed unused import

--- src/com/topologi/diffx/event/impl/OpenElementEventNSImpl.java
+++ src/com/topologi/diffx/event/impl/OpenElementEventNSImpl.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 
 import javax.xml.XMLConstants;
 
-import com.topologi.diffx.util.Constants;
 import com.topologi.diffx.event.DiffXEvent;
 import com.topologi.diffx.event.OpenElementEvent;
 import com.topologi.diffx.xml.XMLWriter;
