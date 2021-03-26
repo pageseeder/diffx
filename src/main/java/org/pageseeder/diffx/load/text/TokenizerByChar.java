0b3d31980b0db200363455ac89ef9db904d31683
christophe <tyenki@gmail.com>
2021-03-26 14:15:49 +1100

    Remove unused optimisation

--- src/main/java/org/pageseeder/diffx/load/text/TokenizerByChar.java
+++ src/main/java/org/pageseeder/diffx/load/text/TokenizerByChar.java
@@ -37,11 +37,6 @@ import org.pageseeder.diffx.event.impl.SpaceEvent;
 public final class TokenizerByChar implements TextTokenizer {
 
   /**
-   * Map characters to events in order to recycle events as they are created.
-   */
-  private final Map<Character, TextEvent> recycling = new HashMap<>();
-
-  /**
    * Creates a new tokenizer.
    */
   public TokenizerByChar() {
@@ -52,16 +47,14 @@ public final class TokenizerByChar implements TextTokenizer {
     if (seq == null) throw new NullPointerException("Character sequence is null");
     if (seq.length() == 0) return Collections.emptyList();
     List<TextEvent> events = new ArrayList<>(seq.length());
-    Character c = null;
+    Character c;
     for (int i=0; i < seq.length(); i++) {
-      c = Character.valueOf(seq.charAt(i));
-      TextEvent e = this.recycling.get(c);
-      if (e == null) {
-        if (Character.isWhitespace(c.charValue())) {
-          e = SpaceEvent.getInstance(c);
-        } else {
-          e = new CharactersEvent(c+"");
-        }
+      c = seq.charAt(i);
+      TextEvent e;
+      if (Character.isWhitespace(c)) {
+        e = SpaceEvent.getInstance(c);
+      } else {
+        e = new CharactersEvent(Character.toString(c));
       }
       events.add(e);
     }
