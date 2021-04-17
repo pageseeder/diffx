106b7d95e5b203a06fcd6479e70593cc03edee32
christophe <tyenki@gmail.com>
2021-04-17 11:50:02 +1000

    Add `toString` to `TokenType`

--- src/main/java/org/pageseeder/diffx/token/TokenType.java
+++ src/main/java/org/pageseeder/diffx/token/TokenType.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2010-2015 Allette Systems (Australia)
- * http://www.allette.com.au
+ * Copyright (c) 2010-2021 Allette Systems (Australia)
+ *    http://www.allette.com.au
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -55,4 +55,8 @@ public enum TokenType {
    */
   OTHER;
 
+  @Override
+  public String toString() {
+    return this.name().toLowerCase().replace('_', '-');
+  }
 }
